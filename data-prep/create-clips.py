0e00ea339be7345b8e6652bd77ae6472ff7b106b
LeonardoViotti <leonardoviotti@gmail.com>
2023-08-02 15:59:21 -0500

    Draft split clips for TDL.

--- data-prep/create-clips.py
+++ data-prep/create-clips.py
@@ -21,13 +21,24 @@ def create_clip(audio_path, st_time, end_time, margin = 3, plot = False):
 
 def create_clips(df, 
                  dest, 
-                 scores_threshold = None, 
                  margin = 3, 
                  path_col = 'file', st_col = 'start_time', 
                  ed_col = 'end_time', score_col = 'score'):
+    """Crate clipped audio files based on input data with 
     
-    if scores_threshold is not None:
-        df = df[df[score_col] >= scores_threshold]
+    Args:
+        df (pd.DataFrame): Reference data frame containing:
+            [path_col] : full audio file path
+            [st_col] : clip start time in seconds
+            [ed_col] :  clip end time in seconds
+            [score_col] : clip classifier score
+        dest (str): destination folder
+        margin (int, optional): Clip padding on both ends. Defaults to 3.
+        path_col (str, optional): Dataframe file path column name. Defaults to 'file'.
+        st_col (str, optional):  Dataframe start time column name. Defaults to 'start_time'.
+        ed_col (str, optional): _ Dataframe end time column name. Defaults to 'end_time'.
+        score_col (str, optional):  Dataframe scores column name. Defaults to 'score'.
+    """
     
     for _,row in tqdm(df.iterrows()):
         file = row[path_col]
@@ -42,19 +53,21 @@ def create_clips(df,
         clip.save(os.path.join(dest, dest_filename))
 
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("data", type=str, help = 'Absolute path to csv containing clips to be created')
-    parser.add_argument("--dest", type=str, default='tdl-clips', help = 'Destination folder.')
+    parser.add_argument("--dest", type=str, help = 'Destination folder.')
     
-    parser.add_argument("--min-score", dest = 'scoreth', type=int, help = 'Minimum score to be included')
+    # parser.add_argument("--min-score", dest = 'scoreth', type=int, help = 'Minimum score to be included')
+    parser.add_argument("--top", type=int, help = 'Number of clips to created.')
+    
     parser.add_argument("--padding", default= 2, type=int, help = 'Padding in secods befores and after clip.')
     
     parser.add_argument("--st-col", dest='st', type=str, default='start_time', help= 'Clip start column on [data].')
     parser.add_argument("--ed-col", dest='ed', type=str, default='end_time', help= 'Clip end column on [data].')
     parser.add_argument("--path-col", dest='path', type=str, default='file', help= 'Full audio file path column.')
     parser.add_argument("--scores-col", dest='scores', type=str, default='score', help= 'Scores column.')
+    parser.add_argument("--dry-run", dest="dry_run", action="store_true", default=False, help = "Don't export outputs.")
 
     
     return parser.parse_args()
@@ -62,30 +75,35 @@ def parse_args():
 if __name__ == "__main__":    
     args = parse_args()
     
-    if args.scoreth is not None:
-        scores_threshold = args.scoreth
-    else:
-        scores_threshold = None
-    
     df = pd.read_csv(args.data)
     
-    create_clips(df, 
-                 args.dest,
-                 scores_threshold,
-                 args.padding,
-                 args.path,
-                 args.st,
-                 args.ed,
-                 args.scores)
+    # Make sure df is sorted by score
+    df = df.sort_values(args.scores)
+    
+    # Filter clips
+    if args.top is not None:
+        df = df.head(args.top)
+    
+    # if scores_threshold is not None:
+    #     df = df[df[score_col] >= scores_threshold]
+    
+    # Parse destiation directory
+    if args.dest is None:
+        prefix = os.path.basename(args.data).split('.')[0]
+        dest_dir =os.path.join(os.path.dirname(args.data), f'{prefix}-tdl-clips/')
+    else:
+        dest_dir = args.dest
+    
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+    
+    # Crate clips
+    if not args.dry_run:
+        create_clips(df, 
+                    dest_dir,
+                    args.padding,
+                    args.path,
+                    args.st,
+                    args.ed,
+                    args.scores)
 
-# dest = '/Users/lviotti/Library/CloudStorage/Dropbox/Work/Kitzes/data/ribbitr-br-sample/temp'
-# margin = 2
-# path_col = 'file'
-# st_col = 'start_time'
-# ed_col = 'end_time'
-# score_col = 'score'
-
-# create_clips(df, 
-#                  args.dest,
-#                  scores_threshold,
-#                  args.padding)
